501c84a3296a0fc87708e74487c72dd977791961
Douglas Vieira da Silva <douglas16722@gmail.com>
2025-01-16 20:20:44 -0300

    Add dropdown icon to Options button

--- web/src/components/controls/TableControls.tsx
+++ web/src/components/controls/TableControls.tsx
@@ -47,6 +47,22 @@ export function TableControls({
               {activeOptionsCount}
             </span>
           )}
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className={`w-4 h-4 transform transition-transform ${
+              isOptionsOpen ? 'rotate-180' : ''
+            }`}
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth={2}
+              d='M19 9l-7 7-7-7'
+            />
+          </svg>
         </button>
 
         <div className='ml-auto'>
