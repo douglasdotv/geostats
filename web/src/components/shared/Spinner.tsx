8460c8694befa4496c1c1825742e74d2b9be3f0b
Douglas Vieira da Silva <douglas16722@gmail.com>
2025-01-16 13:54:26 -0300

    Reduce spinner dimensions

--- web/src/components/shared/Spinner.tsx
+++ web/src/components/shared/Spinner.tsx
@@ -1,7 +1,7 @@
 export function Spinner() {
   return (
     <output
-      className='inline-block h-8 w-8 animate-spin rounded-full border-4 border-current border-t-transparent text-blue-600'
+      className='inline-block h-4 w-4 animate-spin rounded-full border-4 border-current border-t-transparent text-blue-600'
       aria-label='Loading'
     />
   );
