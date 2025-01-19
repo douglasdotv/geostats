bcc2ae7601444cd72afc8c4b1c6c1f246bcb4b38
Douglas Vieira da Silva <douglas16722@gmail.com>
2025-01-19 16:54:08 -0300

    Use constant for Street View base URL
    Replaced the hard-coded Street View URL in `GuessRow` with a new constant from the shared constants module.

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,4 +1,7 @@
 export const GEOGUESSR_PROFILE_LINK =
   'https://www.geoguessr.com/user/5df7ff35328e463ee85aefd7';
 
+export const GOOGLE_STREET_VIEW_BASE_URL =
+  'https://www.google.com/maps/@?api=1&map_action=pano&viewpoint=';
+
 export const ITEMS_PER_PAGE = 15;
